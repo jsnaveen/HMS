c1a63cea443354065b090346234f429b242303b1
Naveen Kumar J <118663883+jsnaveen@users.noreply.github.com>
2023-08-24 10:23:13 +0530

    Update PatientPrescription.groovy

--- com.hospital.managementsystem/grails-app/domain/com/hospital/managementsystem/PatientPrescription.groovy
+++ com.hospital.managementsystem/grails-app/domain/com/hospital/managementsystem/PatientPrescription.groovy
@@ -1,5 +1,8 @@
 package com.hospital.managementsystem
 
+import grails.persistence.Entity
+
+@Entity
 class PatientPrescription {
 
     String medication
@@ -15,8 +18,30 @@ class PatientPrescription {
     static belongsTo = [patientProfile: PatientProfile]
 
     static constraints = {
-        therapistInstructions nullable : true
-        prescriptionNo unique : true
-        therapyMode inList: ['DigitalTherapy' , 'HospitalTherapy']
+        therapistInstructions nullable: true
+        therapyMode inList: ['DigitalTherapy', 'HospitalTherapy']
+    }
+
+    
+    def afterInsert() {
+        createPrescriptionHistory()
+    }
+
+    private void createPrescriptionHistory() {
+        PrescriptionHistory.withNewSession {
+            def history = new PrescriptionHistory(
+                medication: medication,
+                prescriptionNo: prescriptionNo,
+                prescriptionDate: prescriptionDate,
+                therapistName: therapistName,
+                therapistInstructions: therapistInstructions,
+                therapyMode: therapyMode,
+                status: status,
+                dateCreated: dateCreated,
+                lastUpdated: lastUpdated,
+                patientPrescription: this
+            )
+            history.save(flush: true)
+        }
     }
 }
